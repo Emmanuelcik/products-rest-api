d80aaad84668849c3307b983ce2bc1d7eb809879
Emmmanuel Lopez <jesusemmcik@gmail.com>
2024-08-27 23:52:12 -0600

    added validations for the request

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { check, validationResult } from "express-validator";
 import Product from "../models/Product.model.js";
 export const createProduct = async (req: Request, res: Response) => {
   // old way
@@ -6,6 +7,23 @@ export const createProduct = async (req: Request, res: Response) => {
   //   const savedProduct = await product.save();
   //   res.status(201).json(savedProduct);
 
+  // Validation
+
+  await check("name")
+    .notEmpty()
+    .withMessage("Product name is required")
+    .run(req);
+  await check("price")
+    .isNumeric()
+    .withMessage("Invalid price")
+    .notEmpty()
+    .withMessage("Product price is required")
+    .run(req);
+
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   const product = await Product.create(req.body);
   res.status(200).json(product);
 };
